1dd193eedf459d9b8cdcc02aeb1b33e84d56af15
torxu <aitoroterocarpintero@gmail.com>
2016-10-03 12:30:22 +0200

    Donde vivo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,6 @@
 public class HolaMundo{
 	//Metodo principal
 	public static void main (String args[]){
-		System.out.println("Aitor Otero Carpintero");
-		System.out.println("21");
+		System.out.println("Aitor Otero Carpintero, 21, Leon");
 	}
 }
